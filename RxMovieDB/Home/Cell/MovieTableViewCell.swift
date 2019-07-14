27635ad592ea931782a2d0a5d2f10ec9dce1a807
Laura Corssac <lauracorssac@hotmail.com>
2019-07-13 23:20:39 -0300

    adding git ignore

---
+++ RxMovieDB/Home/Cell/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  RxMovieDB
+//
+//  Created by Laura Corssac on 13/07/19.
+//  Copyright © 2019 Laura Corssac. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ RxMovieDB/MovieImage/MoviePosterViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MoviePosterViewModel.swift
+//  RxMovieDB
+//
+//  Created by Laura Corssac on 13/07/19.
+//  Copyright © 2019 Laura Corssac. All rights reserved.
+//
+
+import Foundation
